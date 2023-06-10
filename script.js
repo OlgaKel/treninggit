390ad811e5bf5d3b3f074be0262249d2c2513bbc
OlgaKel <nikkelos@ya.ru>
2023-06-10 12:56:58 +0300

    function

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
 "use strict";
-let students = {
-	'group1': {
-		'subgroup11': ['student111', 'student112', 'student113'],
-		'subgroup12': ['student121', 'student122', 'student123'],
-	},
-	'group2': {
-		'subgroup21': ['student211', 'student212', 'student213'],
-		'subgroup22': ['student221', 'student222', 'student223'],
-	},
-	'group3': {
-		'subgroup31': ['student311', 'student312', 'student313'],
-		'subgroup32': ['student321', 'student322', 'student323'],
-	},
 
-	
-};
-students.group1.subgroup11.push('student114');
-students.group1.subgroup13=[];
-students.group4=[];
-students.group4.subgroup41=[];
-students.group4.subgroup41.push('student411');
-students.group4.subgroup41.push('student412');
-
-console.log(students);
+function getArr(str){
+    let res = str.split();
+	 return res;
+} 
+console.log('abcd');
