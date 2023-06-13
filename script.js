6c8f2d175bf6cf7aa2b4545362f71216a0d8c5ed
OlgaKel <nikkelos@ya.ru>
2023-06-13 17:08:08 +0300

    callbacks

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 "use strict";
-
-function getArr(str){
-    let res = str.split();
-	 return res;
-} 
-console.log('abcd');
+function each(arr, callback) {
+	let result = [];
+	for (let elem of arr) {
+		result.push( callback(elem) ); // вызываем функцию-коллбэк
+	}
+	return result;
+}
+let result = each(['we', 'qa', 'as', 'cx', 'cx'], function(str) {
+	return str.substr(0,1).toUpperCase()+str.substr(1);
+});
+console.log(result);
